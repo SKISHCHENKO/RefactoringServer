7db6972f37c0eb302387869d6b53fcdebc442142
SKishchenko <userg@yandex.ru>
2024-10-03 10:13:26 +0300

    add class Server and methods

---
+++ src/main/java/Net/Server.java
@@ -0,0 +1,123 @@
+package Net;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    final static List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public static void start() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(64);
+        try (final var serverSocket = new ServerSocket(9999)) {
+            while (true) {
+                try {
+                    final var socket = serverSocket.accept();
+                    threadPool.submit(() -> handleRequest(socket, validPaths));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleRequest(Socket socket, List<String> validPaths) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            final var requestLine = in.readLine();
+            if (requestLine == null) {
+                return;
+            }
+            System.out.println(requestLine);
+            final var parts = requestLine.split(" ");
+
+
+            if (parts.length != 3) {
+                sendErrorResponse(out, "400 Bad Request");
+                return;
+            }
+
+            final var path = parts[1];
+
+            if (!validPaths.contains(path)) {
+                System.out.println("404 Not Found");
+                System.out.println("Проверка пути: " + path);
+                sendErrorResponse(out, "404 Not Found");
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            if (!Files.exists(filePath)) {
+                sendErrorResponse(out, "404 Not Found");
+                System.out.println("404 Not Found");
+                return;
+            }
+
+            if (mimeType == null) {
+                sendErrorResponse(out, "415 Unsupported Media Type");
+                return;
+            }
+
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+            } else {
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendErrorResponse(BufferedOutputStream out, String status) throws IOException {
+        System.out.println("Отправка ответа");
+        String notFoundMessage = "<html><body><h1>Файл не найден</h1></body></html>";
+        byte[] notFoundContent = notFoundMessage.getBytes("UTF-8");
+        out.write((
+                "HTTP/1.1 " + status + "\r\n" +
+                        "Content-Type: text/html; charset=UTF-8\r\n" +
+                        "Content-Length: " + notFoundContent.length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.write(notFoundContent);
+        out.flush();
+    }
+}
