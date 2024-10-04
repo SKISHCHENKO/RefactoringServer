d086e546133eff1352198b7b12e416cbaa39a7a3
SKishchenko <userg@yandex.ru>
2024-10-04 23:35:49 +0300

    feature/handlers

--- src/main/java/Net/Main.java
+++ src/main/java/Net/Main.java
@@ -1,10 +1,50 @@
 package Net;
 
 
+import java.nio.charset.StandardCharsets;
+
 public class Main {
+
+    final static String GET = "GET";
+    final static String POST = "POST";
+
     public static void main(String[] args) {
-        final var server = new Server(64);
-        server.listen(9999);
+        final var server = new Server(9999, 64);
+
+        // добавление хендлеров (обработчиков)
+        server.addHandler(GET, "/messages", (request, out) -> {
+            String responseMessage = "New Handler: Method: GET, Path: /messages";
+
+            Request response = new Request(GET, "/messages", "200 OK", "text/plain", responseMessage.getBytes().length, responseMessage, request.getVersion());
+
+            out.write(response.createRequest().getBytes());
+            out.write(responseMessage.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        });
+
+        server.addHandler(GET, "/error", (request, out) -> {
+            String responseMessage = "New Handler: Method: GET, Path: /error" + "\r\n" +
+                    "Message: ERROR";
+
+            Request response = new Request(GET, "/messages", "200 OK", "text/plain", responseMessage.getBytes().length, responseMessage, request.getVersion());
+
+            out.write(response.createRequest().getBytes());
+            out.write(responseMessage.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        });
+
+
+        server.addHandler(POST, "/messages", (Handler) (request, out) -> {
+            String responseMessage = "New Handler: Method: POST, Path: /messages";
+
+            Request response = new Request(POST, "/messages", "200 OK", "text/plain", responseMessage.getBytes().length, responseMessage, request.getVersion());
+
+            out.write(response.createRequest().getBytes());
+            out.write(responseMessage.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        });
+
+        server.listen();
 
     }
 }
--- src/main/java/Net/Server.java
+++ src/main/java/Net/Server.java
@@ -9,24 +9,32 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    final private int PORT;
     final private ExecutorService threadPool;
     final static List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    final static Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+    final static String GET = "GET";
+    final static String POST = "POST";
 
-    public Server(int number) {
-        threadPool = Executors.newFixedThreadPool(number);;
+    public Server(int port, int numberThreads) {
+        this.PORT = port;
+        this.threadPool = Executors.newFixedThreadPool(numberThreads);
     }
 
-    public void listen(int port) {
-        try (final var serverSocket = new ServerSocket(port)) {
+    public void listen() {
+        try (final var serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 try {
                     final var socket = serverSocket.accept();
-                    threadPool.submit(() -> handleRequest(socket, validPaths));
+                    threadPool.submit(() -> defaultHandler(socket, validPaths));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -36,70 +44,71 @@ public class Server {
         }
     }
 
-    private  void handleRequest(Socket socket, List<String> validPaths) {
-        try (
-                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final var out = new BufferedOutputStream(socket.getOutputStream());
-        ) {
+    private void defaultHandler(Socket socket, List<String> validPaths) {
+        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final var out = new BufferedOutputStream(socket.getOutputStream())) {
+
+
             final var requestLine = in.readLine();
+            System.out.println("Request: " + requestLine);
+
             if (requestLine == null) {
                 return;
             }
-            System.out.println("request: "+requestLine);
+
             final var parts = requestLine.split(" ");
-
-
             if (parts.length != 3) {
-                sendErrorResponse(out, "400 Bad Request");
+                sendResponse(out, new Request("400 Bad Request"));
                 return;
             }
 
+            final var method = parts[0];
             final var path = parts[1];
+            final var version = parts[2];
 
-            if (!validPaths.contains(path)) {
-                sendErrorResponse(out, "404 Not Found");
+            // Обработка через динамические хендлеры
+            if (handlers.containsKey(method) && handlers.get(method).containsKey(path)) {
+                if (!new Request("").allowedMethods.contains(method)) {
+                    // Если метод не разрешен, отправляем ответ с ошибкой 405 Method Not Allowed
+                    sendResponse(out, new Request("405 Method Not Allowed"));
+                    return;
+                }
+                // Получаем и обрабатываем запрос с допустимым методом
+                Handler handler = handlers.get(method).get(path);
+                handler.handle(new Request(method, path, "200 OK", "text/html", version), out);
                 return;
             }
 
+            // Обработка статических файлов
             final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
-
             if (!Files.exists(filePath)) {
-                sendErrorResponse(out, "404 Not Found");
-                System.out.println("404 Not Found");
+                sendResponse(out, new Request("404 Not Found"));
                 return;
             }
 
+            final var mimeType = Files.probeContentType(filePath);
             if (mimeType == null) {
-                sendErrorResponse(out, "415 Unsupported Media Type");
+                sendResponse(out, new Request("415 Unsupported Media Type"));
                 return;
             }
 
             if (path.equals("/classic.html")) {
                 final var template = Files.readString(filePath);
-                final var content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
+                final var content = template.replace("{time}", LocalDateTime.now().toString());
 
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
+                // Создаем объект Request с телом контента
+                Request request = new Request(method, path, "200 OK", mimeType, content.getBytes().length, content, version);
+
+                out.write(request.createRequest().getBytes());
+                out.write(content.getBytes());
                 out.flush();
             } else {
-                final var length = Files.size(filePath);
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
+                final var contentLength = (int) Files.size(filePath);
+
+                // Создаем объект Request без тела контента, т.к. будем копировать файл напрямую
+                Request request = new Request(method, path, "200 OK", mimeType, contentLength, version);
+
+                out.write(request.createRequest().getBytes());
                 Files.copy(filePath, out);
                 out.flush();
             }
@@ -108,17 +117,30 @@ public class Server {
         }
     }
 
-    private static void sendErrorResponse(BufferedOutputStream out, String status) throws IOException {
-        String notFoundMessage = "<html><body><h1>Файл не найден</h1></body></html>";
-        byte[] notFoundContent = notFoundMessage.getBytes("UTF-8");
-        out.write((
-                "HTTP/1.1 " + status + "\r\n" +
-                        "Content-Type: text/html; charset=UTF-8\r\n" +
-                        "Content-Length: " + notFoundContent.length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        out.write(notFoundContent);
+    private static void sendResponse(BufferedOutputStream out, Request request) throws IOException {
+        // В случае ошибки добавляем сообщение об ошибке как HTML-страницу
+        if (request.getHeader().startsWith("4") || request.getHeader().startsWith("5")) {
+            String errorMessage = "<html><body><h1>" + request.getHeader() + "</h1></body></html>";
+            byte[] errorContent = errorMessage.getBytes();
+
+            String errorResponse = request.getVersion() + " " + request.getHeader() + "\r\n" +
+                    "Content-Type: text/html" + "\r\n" +
+                    "Content-Length: " + errorContent.length + "\r\n" +
+                    "Connection: close" + "\r\n" +
+                    "\r\n";
+            out.write(errorResponse.getBytes());
+            out.write(errorContent);
+        }
+        else{
+            String response = request.createRequest();
+            out.write(response.getBytes());
+            if (request.getContentBody() != null) {
+                out.write(request.getContentBody().getBytes());
+            }
+        }
         out.flush();
+    }
+    public void addHandler(String method, String path, Handler handler) {
+        handlers.computeIfAbsent(method, k -> new ConcurrentHashMap<>()).put(path, handler);
     }
 }
